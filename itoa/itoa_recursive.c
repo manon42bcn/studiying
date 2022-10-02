beaa05feb3c3a698b82471dd70596110b238fff9
manon42bcn <97629287+manon42bcn@users.noreply.github.com>
2022-10-02 15:29:16 +0200

    itoa recursive

---
+++ itoa/itoa_recursive.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   itoa_recursive.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mporras- <manon42bcn@yahoo.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/02 15:11:01 by mporras-          #+#    #+#             */
+/*   Updated: 2022/10/02 15:11:40 by mporras-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void ft_itoa_build(long int nbr, int *steps, char **rst)
+{
+	if (nbr < 10)
+	{
+		*rst = (char *)malloc(sizeof(char) * (steps[0] + 2));
+		(*rst)[steps[0] + 1] = '\0';
+		if (steps[1] == 1)
+			(*rst)[0] = '-';
+		(*rst)[steps[1]] = (nbr % 10) + '0';
+		steps[1]+=1;
+		return ;
+	}
+	steps[0]++;
+	ft_itoa_build(nbr / 10, steps, rst);
+	(*rst)[steps[1]] = (nbr % 10) + '0';
+	steps[1]++;
+}
+
+char *ft_itoa(int n)
+{
+	char	*rst;
+	int		stp_ind[2];
+
+	stp_ind[0] = 0;
+	stp_ind[1] = 0;
+	rst = NULL;
+	if (n < 0)
+	{
+		stp_ind[0] = 1;
+		stp_ind[1] = 1;
+		ft_itoa_build((long int)n * -1, &stp_ind[0], &rst);
+	}
+	else
+		ft_itoa_build((long int)n, &stp_ind[0], &rst);
+	return (rst);
+}
+
+int main()
+{
+	char *toItoa;
+
+	toItoa = ft_itoa(INT_MIN);
+	printf("%d <- min [ %s ] <- str\n", INT_MIN, toItoa);
+	free(toItoa);
+	toItoa = ft_itoa(INT_MAX);
+	printf("%d <- max [ %s ] <- str\n", INT_MAX, toItoa);
+	free(toItoa);
+	return (0);
+}
